6e4f5b2c656a0ae012bc99fb94baffe3eb389d70
urirob <91110790+urirob@users.noreply.github.com>
2022-02-27 19:58:43 +0530

    Add files via upload

---
+++ output_code4.py
@@ -0,0 +1,62 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:\Users\DELL\OneDrive\Desktop\assgn_04.py =============
+---------OUTPUT-1---------
+shifted disk 1 selected the rod A destination of rod C
+shifted disk 2 selected the rod A destination of rod B
+shifted disk 1 selected the rod C destination of rod B
+shifted disk 3 selected the rod A destination of rod C
+shifted disk 1 selected the rod B destination of rod A
+shifted disk 2 selected the rod B destination of rod C
+shifted disk 1 selected the rod A destination of rod C
+
+
+---------OUTPUT-2---------
+Enter the size of pascals triangle you want: 5
+PASCAL TRIANGLE USING LOOPS
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+
+
+PASCAL TRIANGLE USING RECURSSIONS
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+ 1 4 6 4 1 
+
+
+---------OUTPUT-3---------
+enter first no.:17
+enter second no.:4
+part-(a):Checking whether the quotient and remainder is a callable value or not
+False
+False
+(b) All the values are non zero
+(c) Filtered out numbers that are greater than 4 are : [8, 5, 6, 9, 6, 10, 7]
+(d) Set: {5, 6, 7, 8, 9, 10}
+(e) Immutable set: frozenset({5, 6, 7, 8, 9, 10})
+(f) Hash value of the maximum value from the above set: 10
+
+
+---------OUTPUT-4---------
+Object created
+The name of the student it TARUNDEEP SINGH and SID is 20103105.
+Object destroyed
+
+
+---------OUTPUT-5---------
+(a)The updated salary of Mehak is 70000 
+(b)Record of Viren deleted
+
+---------OUTPUT-6---------
+Enter the word: nurat
+Enter a word using the exact same letters to test your friendship: tarun
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
+>>> 
